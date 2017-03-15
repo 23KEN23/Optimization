95c83c04b0e443eabebf85f1cc4d542e0e17bac0
karencronk <kec92@cornell.edu>
2017-03-15 19:59:49 -0400

    update GUI
    radio button

--- MatplotGUI.py
+++ MatplotGUI.py
@@ -7,6 +7,7 @@ Created on Fri Mar  3 16:05:10 2017
 #python Desktop/MITRE_AMPL/MatplotGUI.py
 
 import numpy as np
+
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
@@ -20,7 +21,7 @@ dist0 = 200 #initial max distance value
 
 
 fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.25, bottom=0.25)
+plt.subplots_adjust(left=0.25, bottom=0.251)
 
 #that sine wave plot
 t = np.arange(0.0, 1.0, 0.001)
@@ -30,8 +31,8 @@ plt.axis([0, 1, -10, 10])
 
 #slider bars
 axcolor = 'lightgrey'
-slideD_axes = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
-slideA_axes = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+slideD_axes = plt.axes   ([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+slideA_axes = plt.axes   ([0.25, 0.1,  0.65, 0.03], facecolor=axcolor)
 slideDist_axes = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=axcolor)
 
 slideD = Slider(slideD_axes, 'District Courts', 0, TotalDistricts, valinit=d0,valfmt='%0.0f')
@@ -42,7 +43,7 @@ def update(val):
     maxOpenD = slideD.val
     maxOpenA = slideA.val
     maxDist = slideDist.val
-    #make this call function to run gurobi/optimization model
+    #remove this and replace with visualization
     l.set_ydata(maxOpenA*np.sin(2*np.pi*maxOpenD*t))
     fig.canvas.draw_idle()
 
@@ -50,18 +51,20 @@ slideD.on_changed(update)
 slideA.on_changed(update)
 
 def RunModel(val):
-    #maxOpenD = slideD.val
-    #maxOpenA = slideA.val
+    maxOpenD = slideD.val
+    maxOpenA = slideA.val
+    maxDist = slideDist.val
     #make this call function to run gurobi/optimization model
     print('RUN!')
 
 
 #buttons    
-reset_axes = plt.axes([0.8, 0.0, 0.1, 0.04])
-resetButton = Button(reset_axes, 'Reset', color=axcolor, hovercolor='0.975')
+reset_axes = plt.axes([0.8,  0.01, 0.1, 0.04])
+run_axes = plt.axes(  [0.65, 0.01, 0.1, 0.04])
 
-run_axes = plt.axes([0.65, 0.0, 0.1, 0.04])
-runButton = Button(run_axes, 'Run', color='skyblue' , hovercolor='lightblue' )
+resetButton = Button(reset_axes, 'Reset', color=axcolor, hovercolor='0.975')
+runButton   = Button(run_axes, 'Run', color='skyblue' , hovercolor='lightblue' )
+
 runButton.on_clicked(RunModel)
 
 def reset(event):
@@ -71,12 +74,13 @@ def reset(event):
 resetButton.on_clicked(reset)
 
 #radio buttons
-raxes = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(raxes, ('red', 'blue', 'green'), active=0)
+raxes = plt.axes([0.01, .25, 0.1, 0.2], facecolor=axcolor)
+radio = RadioButtons(raxes, ('red','blue'), active=0)
 
 def colorfunc(label):
     l.set_color(label)
     fig.canvas.draw_idle()
+
 radio.on_clicked(colorfunc)
 
 
